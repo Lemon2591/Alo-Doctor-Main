6a3194b710a61a96cc8e2dd3eedc2a5a9c88b1ec
Lemon2591 <89698400+Lemon2591@users.noreply.github.com>
2022-07-31 02:41:49 +0700

    fix

--- client/src/components/common-doctor-patient/register/RegisterComponent.js
+++ client/src/components/common-doctor-patient/register/RegisterComponent.js
@@ -1,5 +1,5 @@
 import { data, post } from "jquery";
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import { NavLink, useHistory } from "react-router-dom";
 import "./RegisterComponent.css";
 
